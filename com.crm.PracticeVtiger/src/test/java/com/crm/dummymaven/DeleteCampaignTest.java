7161ad72924eab64f0b25520cc3d943ff55c7cca
Lenovo <Lenovo@DESKTOP-PG39M5C>
2022-03-23 17:18:46 +0530

    second commit

---
+++ com.crm.PracticeVtiger/src/test/java/com/crm/dummymaven/DeleteCampaignTest.java
@@ -0,0 +1,12 @@
+package com.crm.dummymaven;
+
+import org.testng.annotations.Test;
+
+public class DeleteCampaignTest {
+	@Test
+	public void deleteCampaign()
+	{
+		System.out.println("Campaign deleted");
+	}
+
+}
